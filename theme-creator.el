ccb1d638faf7aaae28ddfc6230ab6b1978a92dde
martin.haesler <martin.haesler@gmail.com>
2014-06-28 22:11:19 +0100

    add init-creator-grid

--- theme-creator.el
+++ theme-creator.el
@@ -3,6 +3,7 @@
 ;;; Code:
 
 (eval-when-compile (require 'cl-lib))
+(require 'widget)
 
 (defvar dark-background nil)
 
@@ -15,7 +16,14 @@
   :group 'theme-creator
   :type 'string)
 
+(defvar  *theme-name*  "")
+
+(defun set-theme-name (name)
+  "set *theme-name* to provided name."
+  (setf *theme-name* name))
+
 (make-variable-buffer-local 'dark-background)
+
 
 (defun dark-background-p ()
   "is background set to dark?"
@@ -29,12 +37,40 @@
   "set background to light"
   (setf dark-background nil))
 
+(defun set-background (str)
+  "set background to light or dark according to supplied string."
+  (if (string=  str "Light")
+      (set-light-background)
+    (set-dark-background)))
+
 (defun theme-creator ()
   ""
   (interactive)
   (select-window (or (get-buffer-window theme-creator-buffer-name)
                      (selected-window)))
-  (switch-to-buffer theme-creator-buffer-name))
+  (switch-to-buffer theme-creator-buffer-name)
+  (init-creator-grid))
+
+
+(defun init-creator-grid ()
+  (widget-insert "some documentation\n\n")
+  (widget-create 'editable-field
+                 :size 20
+                 :notify (lambda (widget &rest ignore)
+                           (set-theme-name (widget-value widget)))
+                 :format "%v"; Text after field!
+                 "Theme Name")
+  (widget-insert "\n\n")
+  (widget-create 'radio-button-choice
+                  :value "Light"
+                  :notify (lambda (widget &rest ignore)
+                            (set-background (widget-value widget))
+                            (message "You set the background to %s."
+                                     (widget-value widget)))
+                  '(item "Light") '(item "Dark"))
+  (widget-insert "\n")
+  (widget-setup)
+  (use-local-map widget-keymap))
 
 
 
