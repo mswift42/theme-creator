7a03b23867d43ad90cbc539b32c2b01a66748310
martin.haesler <martin.haesler@gmail.com>
2014-07-16 19:43:31 +0100

    Add selectedColors fn

--- theme-creator.go
+++ theme-creator.go
@@ -1,6 +1,12 @@
 package emacsthemecreator
 
-import "strconv"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
 
 const test = `'((mainbg . {{.bg1}}) (mainfg . {{ .fg1}}))`
 
@@ -75,12 +81,70 @@ func (r RGB) Darken(factor float64) RGB {
 	return NewRGB(newrgbslice[0], newrgbslice[1], newrgbslice[2])
 }
 
+func decodeTheme(r io.ReadCloser) (*Theme, error) {
+	defer r.Close()
+	var theme Theme
+	err := json.NewDecoder(r).Decode(&theme)
+	return &theme, err
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/savetheme", saveThemeHandler)
+}
+func selectedColors(r *http.Request) map[string]string {
+	facemap := make(map[string]string)
+	faces := []string{"deffacebg", "deffacefg", "builtinface", "stringface",
+		"keywordface", "functionnameface", "commentface", "variableface",
+		"typeface", "constantface", "warningface"}
+	for _, i := range faces {
+		facemap[i] = r.FormValue(i)
+	}
+	facemap["themename"] = r.FormValue("themename")
+	return facemap
+}
+func saveThemeHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmap := selectedColors(r)
+	fmt.Fprintf(w, fmap["warningface"])
+	// themefile := template.Must(template.New("theme").ParseFiles("themetemplate.txt"))
+	// if err := themefile.Execute(w, fmap); err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
+
+}
+
+func handleTheme(r *http.Request) (interface{}, error) {
+	switch r.Method {
+	case "POST":
+		theme, err := decodeTheme(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println(theme)
+
+	case "GET":
+		var theme Theme
+		return theme, nil
+	}
+
+	return nil, fmt.Errorf("method not implemented")
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	val, err := handleTheme(r)
+	if err == nil {
+		fmt.Fprintf(w, "Success")
+		fmt.Println(val)
+	}
+
+}
+
 // func main() {
-//	temp := template.Must(template.New("test").Parse(test))
-//	temp.Execute(os.Stdout, map[string]interface{}{"bg1": "\"#000000\"",
-//		"fg1": "\"#ffffff\""})
-//	fmt.Println()
-//	fmt.Println(rgbToHex(255, 255, 255))
-//	fmt.Println(rgbToHex(0, 0, 0))
-//	fmt.Println(122 * 120 / 100)
+// 	temp := template.Must(template.New("test").Parse(test))
+// 	temp.Execute(os.Stdout, map[string]interface{}{"bg1": "\"#000000\"",
+// 		"fg1": "\"#ffffff\""})
+// 	fmt.Println()
+// 	fmt.Println(rgbToHex(255, 255, 255))
+// 	fmt.Println(rgbToHex(0, 0, 0))
+// 	fmt.Println(122 * 120 / 100)
 // }
