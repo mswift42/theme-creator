4a19287ff242355bfa72aaa3b933582c17e1d306
martin.haesler <martin.haesler@gmail.com>
2014-07-20 09:52:21 +0100

    remove redundant resource

--- webapp/app/js/app.js
+++ webapp/app/js/app.js
@@ -3,15 +3,20 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
+    'ngRoute',
+    'ngResource',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
     'myApp.controllers',
     'colorpicker.module',
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/themecreator', {templateUrl: 'partials/themecreator.html', controller: 'Cpick'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'Cpick'});
-  $routeProvider.otherwise({redirectTo: '/themecreator'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/themecreator', {templateUrl: 'partials/themecreator.html', controller: 'Cpick'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'Cpick'});
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]).
+    factory('Theme',function($resource) {
+        var Theme = $resource('/');
+        return Theme;
+    });
