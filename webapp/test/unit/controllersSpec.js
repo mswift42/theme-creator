1dbedc1fd82b1cf5e4008631bedfa4b61f40a72f
martin.haesler <martin.haesler@gmail.com>
2014-12-02 20:12:19 +0000

    add test for darkBg fn

--- webapp/test/unit/controllersSpec.js
+++ webapp/test/unit/controllersSpec.js
@@ -7,11 +7,11 @@ describe('controllers', function(){
 
 
 
-  it('Cpick controller should exist.', inject(function($controller) {
-    //spec body
-    var Cpick = $controller('Cpick', { $scope: {} });
-      expect(Cpick).toBeDefined();
-  }));
+    it('Cpick controller should exist.', inject(function($controller) {
+        //spec body
+        var Cpick = $controller('Cpick', { $scope: {} });
+        expect(Cpick).toBeDefined();
+    }));
     it('$scope.adjustbg should be set to false.', inject(function($controller, $rootScope) {
 
         var scope = $rootScope.$new();
@@ -21,6 +21,18 @@ describe('controllers', function(){
         expect(scope.prevlang === 'ruby');
         expect(scope.faces.deffacefg === "#303030");
     }));
+    it('darkBg function should return true if supplied color is dark',
+       inject(function($controller, $rootScope) {
+           var scope = $rootScope.$new();
+           var Cpick = $controller('Cpick', {$scope: scope});
+           scope.$digest();
+           expect(scope.darkBg('#000000') === true);
+           expect(scope.darkBg('#ffffff') === false);
+           expect(scope.darkBg('#002b36') === true);
+           expect(scope.darkBg('#586e75') === true);
+           expect(scope.darkBg('#fdf6e3') === false);
+           expect(scope.darkBg('#87e37e') === false);
+       }));
 
 
 
