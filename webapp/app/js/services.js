5838d580d283c8120d62e7bf44f622f9d99aaef5
martin.haesler <martin.haesler@gmail.com>
2015-03-18 17:59:18 +0000

    add lstorage service

--- webapp/app/js/services.js
+++ webapp/app/js/services.js
@@ -5,3 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
+angular.module('myApp.services',[])
+    .factory('lstorage', function() {
+        
+        var storageService = {};
+        storageService.name = 'mswift42themecreator';
+        storageService.loadFaces = function() {
+            localStorage.getItem(storageService.name);
+        };
+        storageService.setFaces = function(faces) {
+            localStorage.setItem(storageService.name, faces);
+        };
+        return storageService;
+    });
