4e7b26800c01e1171be7d68c5427782e2364e914
martin.haesler <martin.haesler@gmail.com>
2015-04-01 17:41:46 +0100

    add oldlace / warmnight to presetService

--- webapp/app/js/services.js
+++ webapp/app/js/services.js
@@ -23,4 +23,27 @@ angular.module('myApp.services',[])
             localStorage.removeItem(storageService.name);
         };
         return storageService;
+    })
+    .factory('presets', function() {
+
+        var presetService = {};
+        presetService.warmNight = function () {
+            return {deffacefg : "#b1b1b1", deffacebg : "#292424",
+                        keywordface : "#96905f", builtinface : "#71a46c",
+                        stringface : "#71a19", functionnameface : "#b680b1",
+                        typeface : "#8b8fc6", constantface : "#bd845f",
+                        variableface : "#c27d7b", warningface : "#e81050",
+                    commentface : "#5d5a58"
+                   };
+        };
+        presetService.oldlace = function() {
+            return {deffacefg : "#525252", deffacebg : "#fdf5e6",
+                    keywordface : "#3f567b", builtinface : "#7b4135",
+                        stringface : "#305f5e", functionnameface : "#714355",
+                        typeface : "#634575", constantface : "#64502f",
+                        variableface : "#3f5b32", warningface : "#fa0c0c",
+                    commentface : "#949494"
+                   };
+        };
+        presetService.
     });
